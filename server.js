16cc1e94dd397bde9005ec2d31293dbdecfcd35e
Kuba Walinski <jakub.walinski@pl.abb.com>
2014-11-17 06:48:17 +0100

    Saving new questions and returning all questions.

--- server.js
+++ server.js
@@ -2,14 +2,33 @@ var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+var storage = {};
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
+// middleware to use for all requests
+router.use(function(req, res, next) {
 	console.log("request came in!");
+	next(); // make sure we go to the next routes and don't stop here
+});
+
+router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });	
 });
+
+router.route('/questions')
+	.post(function(req, res) {
+		var id = req.body.id;
+		var question = req.body.question;
+		storage[id] = question;
+		res.json({ message: "Question saved" });
+	})
+	.get(function(req, res) {
+		res.json(storage);
+	});
 
 app.use('/api', router);
 
